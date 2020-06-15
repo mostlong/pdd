c8954b8d4f5dc29cebeb1f84afd093f75ecf199e
mostlong <895179286@qq.com>
2020-06-15 18:40:49 +0800

    第二次提交pdd

--- build/utils.js
+++ build/utils.js
@@ -22,6 +22,14 @@ exports.cssLoaders = function (options) {
     }
   }
 
+  // rem编译，（新加px2）
+  const px2remLoader = {
+    loader: 'px2rem-loader',
+    options: {
+      remUnit: 75 //以750设计稿为基准
+    }
+  }
+
   const postcssLoader = {
     loader: 'postcss-loader',
     options: {
@@ -31,7 +39,7 @@ exports.cssLoaders = function (options) {
 
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader, px2remLoader] : [cssLoader, px2remLoader]
 
     if (loader) {
       loaders.push({
